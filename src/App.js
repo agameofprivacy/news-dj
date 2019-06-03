70d7af42e668b614cefc58fcb075c3886acbd893
Eddie Chen <agameofprivacy@gmail.com>
2019-06-03 13:23:18 +0800

    fixed programming details

--- src/App.js
+++ src/App.js
@@ -49,13 +49,28 @@ function App() {
 
   var items = [
     {
-      name: "公視晚間新聞",
+      name: "公視早安新聞",
       description: "識消乎。龍前小聲。片社演所了小很……日常灣男關轉大來爭到？有為天美面列了該停告友下光，電來推的現門定子父快想由教者快沒度沒一成想係。腦斷了聲舉，著早路方房；明為事海經是及思人許，兒物利發克建想住：青提則走發來備查過是假過心提的，美主法見我。南基到縣來了次房來愛品石紅。",
-      previewUrl: "https://i.ytimg.com/vi/gLSDD3-zeks/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDT1gvuTUeOyGEuu_Ya1A2e4IrT3Q",
+      previewUrl: "https://scontent.ftpe4-2.fna.fbcdn.net/v/t31.0-8/13227674_1781318962098600_6764694803977000891_o.jpg?_nc_cat=100&_nc_ht=scontent.ftpe4-2.fna&oh=bf316e0825d4254c5ca122de7e4f7736&oe=5D8C0A25",
       pills: [
         {
           key: "華語",
           value: "chinese"
+        },
+        {
+          key: "字幕",
+          value: "subtitles"
+        }
+      ]
+    },
+    {
+      name: "公視中晝新聞",
+      description: "識消乎。龍前小聲。片社演所了小很……日常灣男關轉大來爭到？有為天美面列了該停告友下光，電來推的現門定子父快想由教者快沒度沒一成想係。腦斷了聲舉，著早路方房；明為事海經是及思人許，兒物利發克建想住：青提則走發來備查過是假過心提的，美主法見我。南基到縣來了次房來愛品石紅。",
+      previewUrl: "https://scontent.ftpe4-1.fna.fbcdn.net/v/t1.0-9/50985884_2542025332479116_2237184473768656896_o.jpg?_nc_cat=106&_nc_ht=scontent.ftpe4-1.fna&oh=17def2ed0044971f8bd0d0a322a0f689&oe=5D53E212",
+      pills: [
+        {
+          key: "台語",
+          value: "taiwanese"
         },
         {
           key: "字幕",
@@ -79,7 +94,97 @@ function App() {
       ]
     },
     {
-      name: "公視晚間新聞",
+      name: "公視暗時新聞",
+      description: "識消乎。龍前小聲。片社演所了小很……日常灣男關轉大來爭到？有為天美面列了該停告友下光，電來推的現門定子父快想由教者快沒度沒一成想係。腦斷了聲舉，著早路方房；明為事海經是及思人許，兒物利發克建想住：青提則走發來備查過是假過心提的，美主法見我。南基到縣來了次房來愛品石紅。",
+      previewUrl: "https://i.ytimg.com/vi/gLSDD3-zeks/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDT1gvuTUeOyGEuu_Ya1A2e4IrT3Q",
+      pills: [
+        {
+          key: "台語",
+          value: "taiwanese"
+        },
+        {
+          key: "字幕",
+          value: "subtitles"
+        }
+      ]
+    },
+    {
+      name: "公視手語新聞",
+      description: "識消乎。龍前小聲。片社演所了小很……日常灣男關轉大來爭到？有為天美面列了該停告友下光，電來推的現門定子父快想由教者快沒度沒一成想係。腦斷了聲舉，著早路方房；明為事海經是及思人許，兒物利發克建想住：青提則走發來備查過是假過心提的，美主法見我。南基到縣來了次房來愛品石紅。",
+      previewUrl: "https://i.ytimg.com/vi/gLSDD3-zeks/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDT1gvuTUeOyGEuu_Ya1A2e4IrT3Q",
+      pills: [
+        {
+          key: "手語",
+          value: "sign"
+        },
+        {
+          key: "字幕",
+          value: "subtitles"
+        }
+      ]
+    },
+    {
+      name: "公視英語新聞",
+      description: "識消乎。龍前小聲。片社演所了小很……日常灣男關轉大來爭到？有為天美面列了該停告友下光，電來推的現門定子父快想由教者快沒度沒一成想係。腦斷了聲舉，著早路方房；明為事海經是及思人許，兒物利發克建想住：青提則走發來備查過是假過心提的，美主法見我。南基到縣來了次房來愛品石紅。",
+      previewUrl: "https://i.ytimg.com/vi/gLSDD3-zeks/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDT1gvuTUeOyGEuu_Ya1A2e4IrT3Q",
+      pills: [
+        {
+          key: "English",
+          value: "english"
+        },
+        {
+          key: "字幕",
+          value: "subtitles"
+        }
+      ]
+    },
+    {
+      name: "公視印尼語新聞",
+      description: "識消乎。龍前小聲。片社演所了小很……日常灣男關轉大來爭到？有為天美面列了該停告友下光，電來推的現門定子父快想由教者快沒度沒一成想係。腦斷了聲舉，著早路方房；明為事海經是及思人許，兒物利發克建想住：青提則走發來備查過是假過心提的，美主法見我。南基到縣來了次房來愛品石紅。",
+      previewUrl: "https://i.ytimg.com/vi/gLSDD3-zeks/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDT1gvuTUeOyGEuu_Ya1A2e4IrT3Q",
+      pills: [
+        {
+          key: "Indonesian",
+          value: "indonesian"
+        },
+        {
+          key: "字幕",
+          value: "subtitles"
+        }
+      ]
+    },
+    {
+      name: "公視泰語新聞",
+      description: "識消乎。龍前小聲。片社演所了小很……日常灣男關轉大來爭到？有為天美面列了該停告友下光，電來推的現門定子父快想由教者快沒度沒一成想係。腦斷了聲舉，著早路方房；明為事海經是及思人許，兒物利發克建想住：青提則走發來備查過是假過心提的，美主法見我。南基到縣來了次房來愛品石紅。",
+      previewUrl: "https://i.ytimg.com/vi/gLSDD3-zeks/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDT1gvuTUeOyGEuu_Ya1A2e4IrT3Q",
+      pills: [
+        {
+          key: "ภาษาไทย",
+          value: "thai"
+        },
+        {
+          key: "字幕",
+          value: "subtitles"
+        }
+      ]
+    },
+    {
+      name: "公視越南語新聞",
+      description: "識消乎。龍前小聲。片社演所了小很……日常灣男關轉大來爭到？有為天美面列了該停告友下光，電來推的現門定子父快想由教者快沒度沒一成想係。腦斷了聲舉，著早路方房；明為事海經是及思人許，兒物利發克建想住：青提則走發來備查過是假過心提的，美主法見我。南基到縣來了次房來愛品石紅。",
+      previewUrl: "https://i.ytimg.com/vi/gLSDD3-zeks/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDT1gvuTUeOyGEuu_Ya1A2e4IrT3Q",
+      pills: [
+        {
+          key: "tiếng Việt",
+          value: "vietnamese"
+        },
+        {
+          key: "字幕",
+          value: "subtitles"
+        }
+      ]
+    },
+    {
+      name: "公視新聞全球話",
       description: "識消乎。龍前小聲。片社演所了小很……日常灣男關轉大來爭到？有為天美面列了該停告友下光，電來推的現門定子父快想由教者快沒度沒一成想係。腦斷了聲舉，著早路方房；明為事海經是及思人許，兒物利發克建想住：青提則走發來備查過是假過心提的，美主法見我。南基到縣來了次房來愛品石紅。",
       previewUrl: "https://i.ytimg.com/vi/gLSDD3-zeks/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDT1gvuTUeOyGEuu_Ya1A2e4IrT3Q",
       pills: [
@@ -94,7 +199,7 @@ function App() {
       ]
     },
     {
-      name: "公視晚間新聞",
+      name: "公視公民新聞報",
       description: "識消乎。龍前小聲。片社演所了小很……日常灣男關轉大來爭到？有為天美面列了該停告友下光，電來推的現門定子父快想由教者快沒度沒一成想係。腦斷了聲舉，著早路方房；明為事海經是及思人許，兒物利發克建想住：青提則走發來備查過是假過心提的，美主法見我。南基到縣來了次房來愛品石紅。",
       previewUrl: "https://i.ytimg.com/vi/gLSDD3-zeks/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDT1gvuTUeOyGEuu_Ya1A2e4IrT3Q",
       pills: [
@@ -109,7 +214,7 @@ function App() {
       ]
     },
     {
-      name: "公視晚間新聞",
+      name: "公視新聞實驗室",
       description: "識消乎。龍前小聲。片社演所了小很……日常灣男關轉大來爭到？有為天美面列了該停告友下光，電來推的現門定子父快想由教者快沒度沒一成想係。腦斷了聲舉，著早路方房；明為事海經是及思人許，兒物利發克建想住：青提則走發來備查過是假過心提的，美主法見我。南基到縣來了次房來愛品石紅。",
       previewUrl: "https://i.ytimg.com/vi/gLSDD3-zeks/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDT1gvuTUeOyGEuu_Ya1A2e4IrT3Q",
       pills: [
@@ -122,82 +227,7 @@ function App() {
           value: "subtitles"
         }
       ]
-    },
-    {
-      name: "公視晚間新聞",
-      description: "識消乎。龍前小聲。片社演所了小很……日常灣男關轉大來爭到？有為天美面列了該停告友下光，電來推的現門定子父快想由教者快沒度沒一成想係。腦斷了聲舉，著早路方房；明為事海經是及思人許，兒物利發克建想住：青提則走發來備查過是假過心提的，美主法見我。南基到縣來了次房來愛品石紅。",
-      previewUrl: "https://i.ytimg.com/vi/gLSDD3-zeks/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDT1gvuTUeOyGEuu_Ya1A2e4IrT3Q",
-      pills: [
-        {
-          key: "華語",
-          value: "chinese"
-        },
-        {
-          key: "字幕",
-          value: "subtitles"
-        }
-      ]
-    },
-    {
-      name: "公視晚間新聞",
-      description: "識消乎。龍前小聲。片社演所了小很……日常灣男關轉大來爭到？有為天美面列了該停告友下光，電來推的現門定子父快想由教者快沒度沒一成想係。腦斷了聲舉，著早路方房；明為事海經是及思人許，兒物利發克建想住：青提則走發來備查過是假過心提的，美主法見我。南基到縣來了次房來愛品石紅。",
-      previewUrl: "https://i.ytimg.com/vi/gLSDD3-zeks/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDT1gvuTUeOyGEuu_Ya1A2e4IrT3Q",
-      pills: [
-        {
-          key: "華語",
-          value: "chinese"
-        },
-        {
-          key: "字幕",
-          value: "subtitles"
-        }
-      ]
-    },
-    {
-      name: "公視晚間新聞",
-      description: "識消乎。龍前小聲。片社演所了小很……日常灣男關轉大來爭到？有為天美面列了該停告友下光，電來推的現門定子父快想由教者快沒度沒一成想係。腦斷了聲舉，著早路方房；明為事海經是及思人許，兒物利發克建想住：青提則走發來備查過是假過心提的，美主法見我。南基到縣來了次房來愛品石紅。",
-      previewUrl: "https://i.ytimg.com/vi/gLSDD3-zeks/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDT1gvuTUeOyGEuu_Ya1A2e4IrT3Q",
-      pills: [
-        {
-          key: "華語",
-          value: "chinese"
-        },
-        {
-          key: "字幕",
-          value: "subtitles"
-        }
-      ]
-    },
-    {
-      name: "公視晚間新聞",
-      description: "識消乎。龍前小聲。片社演所了小很……日常灣男關轉大來爭到？有為天美面列了該停告友下光，電來推的現門定子父快想由教者快沒度沒一成想係。腦斷了聲舉，著早路方房；明為事海經是及思人許，兒物利發克建想住：青提則走發來備查過是假過心提的，美主法見我。南基到縣來了次房來愛品石紅。",
-      previewUrl: "https://i.ytimg.com/vi/gLSDD3-zeks/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDT1gvuTUeOyGEuu_Ya1A2e4IrT3Q",
-      pills: [
-        {
-          key: "華語",
-          value: "chinese"
-        },
-        {
-          key: "字幕",
-          value: "subtitles"
-        }
-      ]
-    },
-    {
-      name: "公視晚間新聞",
-      description: "識消乎。龍前小聲。片社演所了小很……日常灣男關轉大來爭到？有為天美面列了該停告友下光，電來推的現門定子父快想由教者快沒度沒一成想係。腦斷了聲舉，著早路方房；明為事海經是及思人許，兒物利發克建想住：青提則走發來備查過是假過心提的，美主法見我。南基到縣來了次房來愛品石紅。",
-      previewUrl: "https://i.ytimg.com/vi/gLSDD3-zeks/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDT1gvuTUeOyGEuu_Ya1A2e4IrT3Q",
-      pills: [
-        {
-          key: "華語",
-          value: "chinese"
-        },
-        {
-          key: "字幕",
-          value: "subtitles"
-        }
-      ]
-    },
+    }
   ];
 
   return (
--- src/components/Queue.js
+++ src/components/Queue.js
@@ -14,16 +14,50 @@ class Queue extends Component {
                         <div className="thumbnail" />
                         <div className="texts column">
                             <h5>公視晚間新聞</h5>
+                            <h6>公視新聞網</h6>
                             <p>識消乎。龍前小聲。片社演所了小很……日常灣男關轉大來爭到？有為天美面列了該停告友下光，電來推的現門定子父快想由教者快沒度沒一成想係。腦斷了聲舉，著早路方房；明...</p>
                         </div>
                     </div>
                     <button className="button button--confirm">加入清單</button>
                     <ul className="queue">
                         <li className="queue__item row">
-                            <img className="queue__item__thumbnail" />
+                            <div className="queue__item__thumbnail"></div>
                             <div className="texts column">
                                 <h5>公視晚間新聞</h5>
-                                <div>公視晚間新聞</div>
+                                <p>2019年6月3日 加入</p>
+                                <p>識消乎。龍前小聲。片社演所了小很……日常灣男關轉大來爭到？有為天美面列了該停告友下光，電來推的現門定子父快想由教者快沒度沒一成想係。腦斷了聲舉，著早路方房；明...</p>
+                            </div>
+                        </li>
+                        <li className="queue__item row">
+                            <div className="queue__item__thumbnail"></div>
+                            <div className="texts column">
+                                <h5>公視晚間新聞</h5>
+                                <p>2019年6月3日 加入</p>
+                                <p>識消乎。龍前小聲。片社演所了小很……日常灣男關轉大來爭到？有為天美面列了該停告友下光，電來推的現門定子父快想由教者快沒度沒一成想係。腦斷了聲舉，著早路方房；明...</p>
+                            </div>
+                        </li>
+                        <li className="queue__item row">
+                            <div className="queue__item__thumbnail"></div>
+                            <div className="texts column">
+                                <h5>公視晚間新聞</h5>
+                                <p>2019年6月3日 加入</p>
+                                <p>識消乎。龍前小聲。片社演所了小很……日常灣男關轉大來爭到？有為天美面列了該停告友下光，電來推的現門定子父快想由教者快沒度沒一成想係。腦斷了聲舉，著早路方房；明...</p>
+                            </div>
+                        </li>
+                        <li className="queue__item row">
+                            <div className="queue__item__thumbnail"></div>
+                            <div className="texts column">
+                                <h5>公視晚間新聞</h5>
+                                <p>2019年6月3日 加入</p>
+                                <p>識消乎。龍前小聲。片社演所了小很……日常灣男關轉大來爭到？有為天美面列了該停告友下光，電來推的現門定子父快想由教者快沒度沒一成想係。腦斷了聲舉，著早路方房；明...</p>
+                            </div>
+                        </li>
+                        <li className="queue__item row">
+                            <div className="queue__item__thumbnail"></div>
+                            <div className="texts column">
+                                <h5>公視晚間新聞</h5>
+                                <p>2019年6月3日 加入</p>
+                                <p>識消乎。龍前小聲。片社演所了小很……日常灣男關轉大來爭到？有為天美面列了該停告友下光，電來推的現門定子父快想由教者快沒度沒一成想係。腦斷了聲舉，著早路方房；明...</p>
                             </div>
                         </li>
                     </ul>
