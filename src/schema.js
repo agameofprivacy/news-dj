a856dafbda06ccd866fd89a13984b31ebb9de01a
Eddie Chen <agameofprivacy@gmail.com>
2019-06-22 12:26:45 +0800

    fix

--- src/schema.js
+++ src/schema.js
@@ -1,14 +1,27 @@
 // programs 新聞節目
 const programs = [
 {
-  "videoID": "",
+  // 晚間新聞
+  "latestVideoIDs": ["l_7qGHDTxUI"]
   "channelId": "",
   "title":"",
   "description":"",
   "uploaded": new Date(),
-  "airTimeStart": new Date(),
-  "airTimeEnd": new Date(),
   "duration": 0,
   "subtitles": true,
-  "language": "",
+  "language": "chinese",
+  "liveVideoId": "_isseGKrquc",
+  "liveStart": new Date(),
+  "liveEnd": new Date(),
 }]
+
+const customVids = [
+  {
+    "videoID": "",
+    "channelId": "",
+    "title":"",
+    "description":"",
+    "uploaded": new Date(),
+    "duration": 0,
+  }
+]
