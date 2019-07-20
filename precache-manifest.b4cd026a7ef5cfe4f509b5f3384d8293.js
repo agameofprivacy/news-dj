10e46a0dac451a9da702a120c4438a0f07ea506d
Eddie Chen <agameofprivacy@gmail.com>
2019-07-20 16:24:17 +0800

    Updates

---
+++ precache-manifest.b4cd026a7ef5cfe4f509b5f3384d8293.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58f991379892a96ce1a85d48de76ff7f",
+    "url": "/news-vj/index.html"
+  },
+  {
+    "revision": "d304a43f42a871d16c9e",
+    "url": "/news-vj/static/css/main.778c70cc.chunk.css"
+  },
+  {
+    "revision": "5d8593acdde06d2aa612",
+    "url": "/news-vj/static/js/2.9235020e.chunk.js"
+  },
+  {
+    "revision": "d304a43f42a871d16c9e",
+    "url": "/news-vj/static/js/main.5f9c2f95.chunk.js"
+  },
+  {
+    "revision": "a5e55f6fb2344fa291e3",
+    "url": "/news-vj/static/js/runtime~main.7ef7d6d7.js"
+  }
+]);
